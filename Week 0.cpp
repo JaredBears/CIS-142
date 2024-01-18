cfe52710d2e84ecf46277700959a5e12ebcb1672
Jared Bears <97640061+JaredBears@users.noreply.github.com>
2024-01-17 18:39:42 -0600

    Week 0

---
+++ Week 0.cpp
@@ -0,0 +1,15 @@
+//Programming Exercise 2-2: Unit 2 Code: Unit 2: Basic Elements of C++
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "CCCCCCCCC       ++              ++\n";
+    cout << "CC              ++              ++\n";
+    cout << "CC        ++++++++++++++  +++++++++++++++\n";
+    cout << "CC        ++++++++++++++  +++++++++++++++\n";
+    cout << "CC              ++              ++\n";
+    cout << "CCCCCCCCC       ++              ++\n";
+    
+    return 0;
+}
